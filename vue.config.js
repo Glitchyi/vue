b8891ee5a04ba83856407efd3b60612005accdd3
Glitchy <advaithnarayananan8@gmail.com>
2022-11-09 20:16:55 +0530

    bro

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    disbaleHostCheck: true,
+    compress: true,
+    port: 443,
+    server : {
+        type: 'https',
+        options: {
+            cert: fs.readFileSync('./certs/cert.pem'),
+            key: fs.readFileSync('./certs/key.pem'),
+          },
+    },
+  },
+})
