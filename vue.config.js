299d37cea08214291cb27b3e1d59723e958c2f25
Glitchy <advaithnarayananan8@gmail.com>
2022-11-12 13:21:43 +0530

    baroo

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-const fs = require('fs')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    disbaleHostCheck: true,
-    compress: true,
-    port: 443,
-    server : {
-        type: 'https',
-        options: {
-            cert: fs.readFileSync('./certs/cert.pem'),
-            key: fs.readFileSync('./certs/key.pem'),
-          },
-    },
-  },
-})
+// const { defineConfig } = require('@vue/cli-service')
+// const fs = require('fs')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     disbaleHostCheck: true,
+//     compress: true,
+//     port: 443,
+//     server : {
+//         type: 'https',
+//         options: {
+//             cert: fs.readFileSync('./certs/cert.pem'),
+//             key: fs.readFileSync('./certs/key.pem'),
+//           },
+//     },
+//   },
+// })
